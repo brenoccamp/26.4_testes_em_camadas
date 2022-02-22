cbf0b9b80b9d9aa6f92e777ccd5ebafce97d2c75
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-22 14:42:55 -0400

    Refactoring movieService.js

--- services/movieService.js
+++ services/movieService.js
@@ -1,11 +1,5 @@
 const MoviesModel = require('../models/movieModel');
 
-const getNewMovie = (movieData) => {
-  const { id, title, directedBy, releaseYear } = movieData;
-
-  return { id, title, directedBy, releaseYear };
-};
-
 const isValid = (title, directedBy, releaseYear) => {
   if (!title || typeof title !== 'string') return false;
   if (!releaseYear || typeof releaseYear !== 'number') return false;
@@ -14,20 +8,13 @@ const isValid = (title, directedBy, releaseYear) => {
   return true;
 };
 
-const getAll = async () => {
-  const moviesData = await MoviesModel
-  .getAll();
-
-  return moviesData.map(getNewMovie);
-};
-
 const create = async ({ title, directedBy, releaseYear }) => {
   const isMovieValid = isValid(title, directedBy, releaseYear);
 
   if (!isMovieValid) return false;
 
   const { id } = await MoviesModel
-  .create({ title, directedBy, releaseYear });
+    .create({ title, directedBy, releaseYear });
 
   return {
     id,
@@ -36,5 +23,4 @@ const create = async ({ title, directedBy, releaseYear }) => {
 
 module.exports = {
   create,
-  getAll,
 };
